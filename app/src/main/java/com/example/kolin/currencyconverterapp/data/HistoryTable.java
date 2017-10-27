04bf829b4104d790b6392a89e7902a14c7603a47
KolinLoures <kolin-1994@mail.ru>
2017-10-27 03:09:45 +0300

    add HistoryTable class

---
+++ app/src/main/java/com/example/kolin/currencyconverterapp/data/HistoryTable.java
@@ -0,0 +1,112 @@
+package com.example.kolin.currencyconverterapp.data;
+
+import android.content.ContentValues;
+
+import java.util.Calendar;
+
+/**
+ * Created by kolin on 26.10.2017.
+ */
+
+public class HistoryTable {
+
+    public static final String TABLE_NAME = "exchange_history_table";
+
+    private static final String ID = "id_exchange";
+    private static final String CURRENCY_FROM = "currency_from";
+    private static final String CURRENCY_TO = "currency_to";
+    private static final String SUM_FROM = "sum_from";
+    private static final String SUM_TO = "sum_to";
+    private static final String RATE = "rate";
+    private static final String TIME = "time_exchange";
+
+
+    public static String createTable() {
+        return "CREATE TABLE "
+                + TABLE_NAME + " ("
+                + ID + " INTEGER PRIMARY KEY,"
+                + CURRENCY_FROM + " TEXT,"
+                + CURRENCY_TO + " TEXT,"
+                + SUM_FROM + " INTEGER,"
+                + SUM_TO + " INTEGER,"
+                + RATE + " FLOAT,"
+                + TIME + " LONG);";
+    }
+
+    public static String selectHistory() {
+        return "SELECT * FROM " + TABLE_NAME
+                + " ORDER BY " + TIME + " DESC";
+    }
+
+    public static String selectHistory(long timeFrom, long timeTo) {
+        return "SELECT * FROM " + TABLE_NAME
+                + " WHERE " + HistoryTable.TIME + " BETWEEN " + timeFrom + " AND " + timeTo
+                + " ORDER BY " + TIME + " DESC";
+    }
+
+    public static String selectHistory(String currencyName) {
+        return "SELECT * FROM " + TABLE_NAME
+                + " WHERE " + HistoryTable.CURRENCY_FROM + " = '" + currencyName + "'"
+                + " OR " + HistoryTable.CURRENCY_TO + " = '"+ currencyName + "'"
+                + " ORDER BY " + TIME + " DESC";
+    }
+
+    public static String selectHistory(String[] currencyNames) {
+
+        String in = createConditionIn(currencyNames);
+
+        return "SELECT * FROM " + TABLE_NAME
+                + " WHERE " + HistoryTable.CURRENCY_FROM + " IN " + in
+                + " OR " + HistoryTable.CURRENCY_TO + " IN " + in
+                + " ORDER BY " + TIME + " DESC";
+    }
+
+    public static String selectHistory(String[] currencyNames, long timeFrom, long timeTo) {
+
+        String in = createConditionIn(currencyNames);
+
+        return "SELECT * FROM " + TABLE_NAME
+                + " WHERE " + "(" + HistoryTable.CURRENCY_FROM + " IN " + in
+                + " OR " + HistoryTable.CURRENCY_TO + " IN " + in + " AND "
+                + "(" + HistoryTable.TIME + " BETWEEN " + timeFrom + " AND " + timeTo + ")"
+                + " ORDER BY " + TIME + " DESC";
+    }
+
+    public static String clearHistory(){
+        return "DELETE FROM " + TABLE_NAME;
+    }
+
+    public static ContentValues getContentValues(String currencyFrom,
+                                                 String currencyTo,
+                                                 int sumFrom,
+                                                 int sumTo,
+                                                 float rate) {
+
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(CURRENCY_FROM, currencyFrom);
+        contentValues.put(CURRENCY_TO, currencyTo);
+        contentValues.put(SUM_FROM, sumFrom);
+        contentValues.put(SUM_TO, sumTo);
+        contentValues.put(RATE, rate);
+        contentValues.put(TIME, Calendar.getInstance().getTimeInMillis());
+
+        return contentValues;
+    }
+
+    private static String createConditionIn(String[] conditions){
+        StringBuilder temp = new StringBuilder("(");
+
+        for (String s: conditions){
+            temp.append("'");
+            temp.append(s);
+            temp.append("'");
+            temp.append(",");
+        }
+
+        temp.append(")");
+
+        return temp.toString();
+    }
+}
+
