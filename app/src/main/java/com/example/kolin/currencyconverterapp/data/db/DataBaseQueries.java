22b33843d9056276c41255f1708de7ac9d361d43
KolinLoures <kolin-1994@mail.ru>
2017-11-01 20:10:14 +0300

    update queries

--- app/src/main/java/com/example/kolin/currencyconverterapp/data/db/DataBaseQueries.java
+++ app/src/main/java/com/example/kolin/currencyconverterapp/data/db/DataBaseQueries.java
@@ -9,6 +9,8 @@ import com.example.kolin.currencyconverterapp.data.entity.CurrencyHistoryEntity;
 import java.util.LinkedList;
 import java.util.List;
 
+import io.reactivex.Observable;
+
 /**
  * Created by kolin on 27.10.2017.
  */
@@ -25,13 +27,19 @@ public class DataBaseQueries implements DAO.HistoryCurrencyDAO, DAO.CurrencyCata
 
 
     @Override
-    public List<CurrencyEntity> getAllCurrency() {
-        return cursorToCurrencyEntityClass(db.getCursor(CurrencyCatalogTable.selectAllCurrencies()));
+    public Observable<CurrencyEntity> getAllCurrency() {
+        return Observable
+                .fromCallable(() -> db.getCursor(CurrencyCatalogTable.selectAllCurrencies()))
+                .map(this::cursorToCurrencyEntityClass)
+                .flatMapIterable(currencyEntities -> currencyEntities);
     }
 
     @Override
-    public List<CurrencyEntity> getAllFavoriteCurrency() {
-        return cursorToCurrencyEntityClass(db.getCursor(CurrencyCatalogTable.selectAllFavoritesCurrencies()));
+    public Observable<CurrencyEntity> getAllFavoriteCurrency() {
+        return Observable
+                .fromCallable(() -> db.getCursor(CurrencyCatalogTable.selectAllFavoritesCurrencies()))
+                .map(this::cursorToCurrencyEntityClass)
+                .flatMapIterable(currencyEntities -> currencyEntities);
     }
 
     @Override
@@ -61,29 +69,44 @@ public class DataBaseQueries implements DAO.HistoryCurrencyDAO, DAO.CurrencyCata
     }
 
     @Override
-    public List<CurrencyHistoryEntity> getHistory() {
-        return cursorToCurrencyHistoryEntity(db.getCursor(HistoryTable.selectHistory()));
+    public Observable<CurrencyHistoryEntity> getHistory() {
+
+        return Observable
+                .fromCallable(() -> db.getCursor(HistoryTable.selectHistory()))
+                .map(this::cursorToCurrencyHistoryEntity)
+                .flatMapIterable(currencyHistoryEntities -> currencyHistoryEntities);
     }
 
     @Override
-    public List<CurrencyHistoryEntity> getHistory(long timeFrom, long timeTo) {
-        return cursorToCurrencyHistoryEntity(db.getCursor(HistoryTable.selectHistory(timeFrom, timeTo)));
+    public Observable<CurrencyHistoryEntity> getHistory(long timeFrom, long timeTo) {
+        return Observable
+                .fromCallable(() -> db.getCursor(HistoryTable.selectHistory(timeFrom, timeTo)))
+                .map(this::cursorToCurrencyHistoryEntity)
+                .flatMapIterable(currencyHistoryEntities -> currencyHistoryEntities);
     }
 
     @Override
-    public List<CurrencyHistoryEntity> getHistory(String currencyName) {
-        return cursorToCurrencyHistoryEntity(db.getCursor(HistoryTable.selectHistory(currencyName)));
+    public Observable<CurrencyHistoryEntity> getHistory(String currencyName) {
+        return Observable
+                .fromCallable(() -> db.getCursor(HistoryTable.selectHistory(currencyName)))
+                .map(this::cursorToCurrencyHistoryEntity)
+                .flatMapIterable(currencyHistoryEntities -> currencyHistoryEntities);
     }
 
     @Override
-    public List<CurrencyHistoryEntity> getHistory(String[] currencyNames) {
-        return cursorToCurrencyHistoryEntity(db.getCursor(HistoryTable.selectHistory(currencyNames)));
+    public Observable<CurrencyHistoryEntity> getHistory(String[] currencyNames) {
+        return Observable
+                .fromCallable(() -> db.getCursor(HistoryTable.selectHistory(currencyNames)))
+                .map(this::cursorToCurrencyHistoryEntity)
+                .flatMapIterable(currencyHistoryEntities -> currencyHistoryEntities);
     }
 
     @Override
-    public List<CurrencyHistoryEntity> getHistory(String[] currencyNames, long timeFrom, long timeTo) {
-        return cursorToCurrencyHistoryEntity(
-                db.getCursor(HistoryTable.selectHistory(currencyNames, timeFrom, timeTo)));
+    public Observable<CurrencyHistoryEntity> getHistory(String[] currencyNames, long timeFrom, long timeTo) {
+        return Observable
+                .fromCallable(() -> db.getCursor(HistoryTable.selectHistory(currencyNames,  timeFrom, timeTo)))
+                .map(this::cursorToCurrencyHistoryEntity)
+                .flatMapIterable(currencyHistoryEntities -> currencyHistoryEntities);
     }
 
     @Override
@@ -92,68 +115,75 @@ public class DataBaseQueries implements DAO.HistoryCurrencyDAO, DAO.CurrencyCata
     }
 
     private List<CurrencyEntity> cursorToCurrencyEntityClass(Cursor cursor) {
-        if (cursor != null && cursor.moveToFirst()) {
+        if (cursor != null && cursor.moveToFirst())
+            try {
 
-            List<CurrencyEntity> temp = new LinkedList<>();
+                List<CurrencyEntity> temp = new LinkedList<>();
 
-            do {
-                int id = cursor.getInt(cursor.getColumnIndex(CurrencyCatalogTable.ID));
-                String name = cursor.getString(cursor.getColumnIndex(CurrencyCatalogTable.NAME));
-                boolean isFavorite = cursor.getInt(cursor.getColumnIndex(CurrencyCatalogTable.IS_FAVORITE)) != 0;
-                long lastUse = cursor.getInt(cursor.getColumnIndex(CurrencyCatalogTable.TIME));
+                do {
+                    int id = cursor.getInt(cursor.getColumnIndex(CurrencyCatalogTable.ID));
+                    String name = cursor.getString(cursor.getColumnIndex(CurrencyCatalogTable.NAME));
+                    boolean isFavorite = cursor.getInt(cursor.getColumnIndex(CurrencyCatalogTable.IS_FAVORITE)) != 0;
+                    long lastUse = cursor.getInt(cursor.getColumnIndex(CurrencyCatalogTable.TIME));
 
-                CurrencyEntity obj = new CurrencyEntity();
+                    CurrencyEntity obj = new CurrencyEntity();
 
-                obj.setId(id);
-                obj.setName(name);
-                obj.setFavorite(isFavorite);
-                obj.setLastUse(lastUse);
+                    obj.setId(id);
+                    obj.setName(name);
+                    obj.setFavorite(isFavorite);
+                    obj.setLastUse(lastUse);
 
-                temp.add(obj);
+                    temp.add(obj);
 
-            } while (cursor.moveToNext());
+                } while (cursor.moveToNext());
 
-            cursor.close();
+                cursor.close();
 
-            return temp;
-        }
+                return temp;
+
+            } finally {
+                cursor.close();
+            }
 
         return null;
     }
 
     private List<CurrencyHistoryEntity> cursorToCurrencyHistoryEntity(Cursor cursor) {
-        if (cursor != null && cursor.moveToNext()) {
+        if (cursor != null && cursor.moveToNext())
+            try {
 
-            List<CurrencyHistoryEntity> temp = new LinkedList<>();
+                List<CurrencyHistoryEntity> temp = new LinkedList<>();
 
-            do {
+                do {
 
-                int id = cursor.getInt(cursor.getColumnIndex(HistoryTable.ID));
-                String from = cursor.getString(cursor.getColumnIndex(HistoryTable.CURRENCY_FROM));
-                String to = cursor.getString(cursor.getColumnIndex(HistoryTable.CURRENCY_TO));
-                int sumFrom = cursor.getInt(cursor.getColumnIndex(HistoryTable.SUM_FROM));
-                int sumTo = cursor.getInt(cursor.getColumnIndex(HistoryTable.SUM_TO));
-                float rate = cursor.getInt(cursor.getColumnIndex(HistoryTable.RATE));
-                long time = cursor.getInt(cursor.getColumnIndex(HistoryTable.TIME));
+                    int id = cursor.getInt(cursor.getColumnIndex(HistoryTable.ID));
+                    String from = cursor.getString(cursor.getColumnIndex(HistoryTable.CURRENCY_FROM));
+                    String to = cursor.getString(cursor.getColumnIndex(HistoryTable.CURRENCY_TO));
+                    int sumFrom = cursor.getInt(cursor.getColumnIndex(HistoryTable.SUM_FROM));
+                    int sumTo = cursor.getInt(cursor.getColumnIndex(HistoryTable.SUM_TO));
+                    float rate = cursor.getInt(cursor.getColumnIndex(HistoryTable.RATE));
+                    long time = cursor.getInt(cursor.getColumnIndex(HistoryTable.TIME));
 
-                CurrencyHistoryEntity obj = new CurrencyHistoryEntity();
+                    CurrencyHistoryEntity obj = new CurrencyHistoryEntity();
 
-                obj.setId(id);
-                obj.setCurrencyFrom(from);
-                obj.setCurrencyTo(to);
-                obj.setSumFrom(sumFrom);
-                obj.setSumTo(sumTo);
-                obj.setRate(rate);
-                obj.setTime(time);
+                    obj.setId(id);
+                    obj.setCurrencyFrom(from);
+                    obj.setCurrencyTo(to);
+                    obj.setSumFrom(sumFrom);
+                    obj.setSumTo(sumTo);
+                    obj.setRate(rate);
+                    obj.setTime(time);
 
-                temp.add(obj);
+                    temp.add(obj);
 
-            } while (cursor.moveToNext());
+                } while (cursor.moveToNext());
 
 
-            return temp;
-        }
+                return temp;
 
+            } finally {
+                cursor.close();
+            }
         return null;
     }
 }
