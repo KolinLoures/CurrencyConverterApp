3fc6894adc81a815303843e44da609bddb506360
KolinLoures <kolin-1994@mail.ru>
2017-11-01 20:21:04 +0300

    reworked rate pojo class

--- app/src/main/java/com/example/kolin/currencyconverterapp/domain/RatePojo.java
+++ app/src/main/java/com/example/kolin/currencyconverterapp/domain/RatePojo.java
@@ -1,6 +1,6 @@
 package com.example.kolin.currencyconverterapp.domain;
 
-import java.util.HashMap;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * Created by kolin on 28.10.2017.
@@ -8,16 +8,35 @@ import java.util.HashMap;
 
 public class RatePojo {
 
-    private HashMap<String, Float> rates;
+    @SerializedName("base")
+    private String currencyFrom;
+    private String currencyTo;
+    private float rate;
 
     public RatePojo() {
     }
 
-    public HashMap<String, Float> getRates() {
-        return rates;
+    public String getCurrencyFrom() {
+        return currencyFrom;
     }
 
-    public void setRates(HashMap<String, Float> rates) {
-        this.rates = rates;
+    public void setCurrencyFrom(String currencyFrom) {
+        this.currencyFrom = currencyFrom;
+    }
+
+    public String getCurrencyTo() {
+        return currencyTo;
+    }
+
+    public void setCurrencyTo(String currencyTo) {
+        this.currencyTo = currencyTo;
+    }
+
+    public float getRate() {
+        return rate;
+    }
+
+    public void setRate(float rate) {
+        this.rate = rate;
     }
 }
