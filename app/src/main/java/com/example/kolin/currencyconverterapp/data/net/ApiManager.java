3221f9905d515303c8a648061801f51410f92a07
KolinLoures <kolin-1994@mail.ru>
2017-11-01 19:22:02 +0300

    add method to get custom gson with deserializers

--- app/src/main/java/com/example/kolin/currencyconverterapp/data/net/ApiManager.java
+++ app/src/main/java/com/example/kolin/currencyconverterapp/data/net/ApiManager.java
@@ -1,6 +1,14 @@
 package com.example.kolin.currencyconverterapp.data.net;
 
-import okhttp3.CacheControl;
+import android.support.annotation.NonNull;
+
+import com.example.kolin.currencyconverterapp.data.ResponseDeserializer;
+import com.example.kolin.currencyconverterapp.data.SupportCurrenciesDeserializer;
+import com.example.kolin.currencyconverterapp.domain.RatePojo;
+import com.example.kolin.currencyconverterapp.domain.SupportCurrencies;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
@@ -27,7 +35,6 @@ public class ApiManager {
     }
 
     private static OkHttpClient getOkHttpClient(){
-        CacheControl forceCache = CacheControl.FORCE_CACHE;
         return new OkHttpClient
                 .Builder()
                 .addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
@@ -37,10 +44,17 @@ public class ApiManager {
     private static Retrofit getRetrofit(){
         return new Retrofit.Builder()
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(getGson()))
                 .client(getOkHttpClient())
                 .baseUrl(Api.BASE_URL)
                 .build();
+    }
+
+    private static Gson getGson() {
+        return new GsonBuilder()
+                    .registerTypeAdapter(SupportCurrencies.class, new SupportCurrenciesDeserializer())
+                    .registerTypeAdapter(RatePojo.class, new ResponseDeserializer())
+                    .create();
     }
 
     private static Api getApi(){
