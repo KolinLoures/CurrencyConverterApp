290beaa4dabe12e93778233149e469f15359a1a6
KolinLoures <kolin-1994@mail.ru>
2017-11-03 02:00:34 +0300

    initialize singletons

--- app/src/main/java/App.java
+++ app/src/main/java/App.java
@@ -1,8 +1,19 @@
 import android.app.Application;
 
+import com.example.kolin.currencyconverterapp.data.db.DataBaseQueries;
+import com.example.kolin.currencyconverterapp.data.net.ApiManager;
+
 /**
  * Created by kolin on 26.10.2017.
  */
 
 public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ApiManager.getInstance();
+        DataBaseQueries.initializeInstanceWithContext(this);
+    }
 }
