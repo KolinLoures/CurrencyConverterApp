29092b3740abaa63cc16fa6b954ec60e11e85985
KolinLoures <kolin-1994@mail.ru>
2017-11-05 01:53:30 +0300

    create base presenter

---
+++ app/src/main/java/com/example/kolin/currencyconverterapp/presentation/BasePresenter.java
@@ -0,0 +1,38 @@
+package com.example.kolin.currencyconverterapp.presentation;
+
+import android.support.annotation.NonNull;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Created by kolin on 05.11.2017.
+ */
+
+public abstract class BasePresenter<V> {
+
+    private WeakReference<V> reference;
+
+    public void bindView(@NonNull V view){
+        if (reference != null && reference.get() != null)
+            throw new RuntimeException("View is bind!");
+        else
+            reference = new WeakReference<>(view);
+    }
+
+    public void unbindView(){
+        if (reference != null) {
+            reference.clear();
+            reference = null;
+        }
+    }
+
+    public boolean isViewBind(){
+        return reference != null && reference.get() != null;
+    }
+
+    public  V getView(){
+        return reference != null ? reference.get() : null;
+    }
+
+
+}
