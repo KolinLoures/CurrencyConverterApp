7d546b7813ebafd9103fc6173402441240739511
KolinLoures <kolin-1994@mail.ru>
2017-11-05 01:52:43 +0300

    add insert to db

--- app/src/main/java/com/example/kolin/currencyconverterapp/domain/GetSupportCurrencies.java
+++ app/src/main/java/com/example/kolin/currencyconverterapp/domain/GetSupportCurrencies.java
@@ -2,12 +2,13 @@ package com.example.kolin.currencyconverterapp.domain;
 
 import com.example.kolin.currencyconverterapp.data.db.DAO;
 import com.example.kolin.currencyconverterapp.data.db.DataBaseQueries;
+import com.example.kolin.currencyconverterapp.data.entity.CurrencyEntity;
 import com.example.kolin.currencyconverterapp.data.net.Api;
 import com.example.kolin.currencyconverterapp.data.net.ApiManager;
 import com.example.kolin.currencyconverterapp.data.preference.PreferenceManager;
-import com.example.kolin.currencyconverterapp.domain.model.SupportCurrenciesPojo;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
 
@@ -15,7 +16,7 @@ import io.reactivex.Observable;
  * Created by kolin on 03.11.2017.
  */
 
-public class GetSupportCurrencies extends BaseObservableUseCase<SupportCurrenciesPojo, Void> {
+public class GetSupportCurrencies extends BaseObservableUseCase<CurrencyEntity, String> {
 
     private Api api;
     private DAO.CurrencyCatalogDAO db;
@@ -27,13 +28,15 @@ public class GetSupportCurrencies extends BaseObservableUseCase<SupportCurrencie
     }
 
     @Override
-    protected Observable<SupportCurrenciesPojo> createObservable(Void params) {
+    protected Observable<CurrencyEntity> createObservable(String params) {
         return api
                 .getRates()
                 .doOnNext(supportCurrenciesPojo -> {
                     List<String> listCurrencies = supportCurrenciesPojo.getListCurrencies();
                     for (String currName : listCurrencies)
                         db.addCurrency(currName);
-                });
+                })
+                .flatMap(supportCurrenciesPojo -> db.getAllCurrency())
+                .delay(2, TimeUnit.SECONDS);
     }
 }
