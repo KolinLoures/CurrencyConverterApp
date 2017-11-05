211a260ed12a1215744cc04b73cd7306dd0d3e01
KolinLoures <kolin-1994@mail.ru>
2017-11-05 03:44:26 +0300

    create

---
+++ app/src/main/java/com/example/kolin/currencyconverterapp/domain/PutRemoveFavoriteCurrency.java
@@ -0,0 +1,43 @@
+package com.example.kolin.currencyconverterapp.domain;
+
+import com.example.kolin.currencyconverterapp.data.db.DAO;
+import com.example.kolin.currencyconverterapp.data.db.DataBaseQueries;
+
+import io.reactivex.Completable;
+
+/**
+ * Created by kolin on 05.11.2017.
+ */
+
+public class PutRemoveFavoriteCurrency extends BaseCompletableUseCase<PutRemoveFavoriteCurrency.PutRemoveFavoriteParams> {
+
+    private DAO.CurrencyCatalogDAO db;
+
+    public PutRemoveFavoriteCurrency() {
+        db = DataBaseQueries.getInstance();
+    }
+
+    @Override
+    protected Completable createCompletable(PutRemoveFavoriteParams param) {
+        return Completable.fromAction(() -> {
+            if (param.remove)
+                db.removeCurrencyFromFavorite(param.id);
+            else
+                db.addCurrencyToFavorite(param.id);
+        });
+    }
+
+    static class PutRemoveFavoriteParams {
+        private int id;
+        private boolean remove;
+
+        private PutRemoveFavoriteParams(int id, boolean remove) {
+            this.remove = remove;
+            this.id = id;
+        }
+
+        public static PutRemoveFavoriteParams getParamObj(int id, boolean remove){
+            return new PutRemoveFavoriteParams(id, remove);
+        }
+    }
+}
