a145fcb18a9fedad84845ef5f0e4312a9846f2d7
KolinLoures <kolin-1994@mail.ru>
2017-12-08 22:45:43 +0300

    add support class

---
+++ app/src/main/java/com/example/kolin/currencyconverterapp/data/model/StringCurrencies.java
@@ -0,0 +1,29 @@
+package com.example.kolin.currencyconverterapp.data.model;
+
+/**
+ * Created by kolin on 08.12.2017.
+ */
+
+public class StringCurrencies {
+    private String name;
+    private boolean flag;
+
+    public StringCurrencies() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+}
