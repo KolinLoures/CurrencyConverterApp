04c9bce4e4860115571256f8f6b9fa224b923b79
KolinLoures <kolin-1994@mail.ru>
2017-12-09 16:25:54 +0300

    add table for preference

---
+++ app/src/main/java/com/example/kolin/currencyconverterapp/data/db/tables/PreferenceTable.java
@@ -0,0 +1,37 @@
+package com.example.kolin.currencyconverterapp.data.db.tables;
+
+import android.content.ContentValues;
+
+/**
+ * Created by kolin on 08.12.2017.
+ */
+
+public class PreferenceTable {
+
+    public static final String TABLE_NAME = "main_preference_table";
+
+    public static String KEY = "key_preference";
+    public static String VALUE = "value_preference";
+
+    public static String createTable(){
+        return "CREATE TABLE "
+                + TABLE_NAME + "("
+                + KEY + " TEXT PRIMARY KEY,"
+                + VALUE + " TEXT)";
+    }
+
+    public static String getPreference(String key){
+        return " SELECT " + VALUE + " FROM " + TABLE_NAME
+                + " WHERE " + KEY + " = " + key;
+    }
+
+    public static ContentValues getContentValues(String key, String value){
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(KEY, key);
+        contentValues.put(VALUE, value);
+
+        return contentValues;
+    }
+
+}
