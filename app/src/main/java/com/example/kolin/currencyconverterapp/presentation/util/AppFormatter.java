2ee97ede21ed4c7de8ef14aba771cc9a35ae053c
KolinLoures <kolin-1994@mail.ru>
2017-12-10 18:41:23 +0300

    renamed

--- app/src/main/java/com/example/kolin/currencyconverterapp/presentation/util/AppFormatter.java
+++ app/src/main/java/com/example/kolin/currencyconverterapp/presentation/util/AppFormatter.java
@@ -1,10 +1,13 @@
 package com.example.kolin.currencyconverterapp.presentation.util;
 
+import android.content.Context;
+import android.text.format.DateUtils;
+
 /**
  * Created by kolin on 19.11.2017.
  */
 
-public class RatesFormatter {
+public class AppFormatter {
 
     public static String formatRate(float f) {
         return String.format("%.2f", f);
@@ -16,6 +19,14 @@ public class RatesFormatter {
 
     public static String formatRate(float from, float to, String nameFrom, String nameTo, String delimiter){
         return formatRate(from)+ " " + nameFrom + delimiter + nameTo + " " + formatRate(to);
+    }
+
+    public static long fromateWeekCloseDatePeriod(Context context, long timeFrom){
+        return timeFrom - DateUtils.WEEK_IN_MILLIS;
+    }
+
+    public static long fromateMonthCloseDatePeriod(Context context, long timeFrom){
+        return timeFrom - DateUtils.DAY_IN_MILLIS * 30;
     }
 
 }
