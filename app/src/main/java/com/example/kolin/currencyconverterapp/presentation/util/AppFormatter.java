9df51e0f0306abec0d79de63907d79229d56118e
KolinLoures <kolin-1994@mail.ru>
2017-12-12 02:50:38 +0300

    update get date methdo

--- app/src/main/java/com/example/kolin/currencyconverterapp/presentation/util/AppFormatter.java
+++ app/src/main/java/com/example/kolin/currencyconverterapp/presentation/util/AppFormatter.java
@@ -1,7 +1,8 @@
 package com.example.kolin.currencyconverterapp.presentation.util;
 
-import android.content.Context;
-import android.text.format.DateUtils;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by kolin on 19.11.2017.
@@ -21,12 +22,22 @@ public class AppFormatter {
         return formatRate(from)+ " " + nameFrom + delimiter + nameTo + " " + formatRate(to);
     }
 
-    public static long fromateWeekCloseDatePeriod(Context context, long timeFrom){
-        return timeFrom - DateUtils.WEEK_IN_MILLIS;
+    public static String formateDate(DateFormat sdf, long time){
+        return sdf.format(new Date(time));
     }
 
-    public static long fromateMonthCloseDatePeriod(Context context, long timeFrom){
-        return timeFrom - DateUtils.DAY_IN_MILLIS * 30;
+    public static String formateDateRange (DateFormat sdf, long timeFrom, long timeTo, String delimiter){
+        return String.format("%s %s %s", formateDate(sdf, timeFrom), delimiter, formateDate(sdf, timeTo));
+    }
+
+    public static Date getDate (int year, int monthOfYear, int dayOfMonth){
+        Calendar now = Calendar.getInstance();
+
+        now.set(Calendar.YEAR, year);
+        now.set(Calendar.MONTH, monthOfYear);
+        now.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+        return now.getTime();
     }
 
 }
